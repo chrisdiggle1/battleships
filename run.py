5ea0b32c0dc84a88c0edcf3159f6ec5663dd9144
chrisdiggle1 <>
2023-11-04 10:30:20 +0000

    Create function to prompt the player to input their guess and handle errors

--- run.py
+++ run.py
@@ -45,9 +45,38 @@ class Board:
                 self.grid[row][col] = "S"
                 ships_placed += 1
 
+    def player_guess(self):
+        """
+        Prompts the player to make their co-ordinate guess. Handles
+        the error if the number or letter is not valid or the guess
+        is outside of the board size. The column input allows both
+        'A' and 'a' inputs. The ord() function is used to get the
+        ASCII code of the characters and covert to integers.
+        """
+        while True:
+            try:
+                row_input = input("Guess a row: ")
+                col_input = input("Guess a column (letter): ").lower()
+                row = int(row_input) - 1
+                col = ord(col_input) - ord('a')
 
-player_board = Board()
-computer_board = Board()
+                if 0 <= row < self.size:
+                    if 0 <= col < self.size:
+                        return (row, col)
+                    else:
+                        print(f"Please enter a letter between a and "
+                              f"{chr(96 + self.size)}")
+                else:
+                    print(f"Please enter a number between 1 and "
+                          f"{self.size}.")
+            except ValueError:
+                print("Enter a valid row number and column letter.")
+
+
+player_board= Board()
+computer_board= Board()
+
+guess_row, guess_col = player_board.player_guess()
 
 print("Your board:")
 player_board.print_board(show_ships=True)
